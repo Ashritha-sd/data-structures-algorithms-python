eddb69c0f075aeebb5c3f79b8c9ecbd975280450
My name <me@example.com>
2024-11-19 11:45:35 -0600

    My solutions to the Array exercise

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -1,9 +1,7 @@
-max = int(input("Enter max number: "))
-
-odd_numbers = []
-
-for i in range(1, max):
-    if i % 2 == 1:
-        odd_numbers.append(i)
-
-print("Odd numbers: ", odd_numbers)
+max_number= int(input())
+odd=[]
+i=1
+while i<=max_number:
+    odd.append(i)
+    i=i+2
+print(odd)

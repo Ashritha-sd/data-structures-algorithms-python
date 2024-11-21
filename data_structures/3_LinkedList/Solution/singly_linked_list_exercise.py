ddcbf8843487b6a6a8e14b284fea26c805e70ab3
My name <me@example.com>
2024-11-20 22:46:32 -0600

    My solution to Singly linked list

--- data_structures/3_LinkedList/Solution/singly_linked_list_exercise.py
+++ data_structures/3_LinkedList/Solution/singly_linked_list_exercise.py
@@ -11,11 +11,10 @@ class LinkedList:
         if self.head is None:
             print("Linked list is empty")
             return
-
         itr = self.head
         llstr = ''
         while itr:
-            llstr += str(itr.data) + ' --> '
+            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
             itr = itr.next
         print(llstr)
 
@@ -62,7 +61,35 @@ class LinkedList:
 
             itr = itr.next
             count += 1
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+        itr=self.head
+        while itr:
+            if itr.data == data_after:
+                node = Node(data_to_insert, itr.next)
+                itr.next = node
+                return
+            itr=itr.next
+        print('Not found the data to insert after')
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+        if self.head.data==data:
+            self.head=self.head.next
+            return
+        #itr1=self.head
+        #itr2=self.head
+        itr=self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next=itr.next.next
+                return
+            itr=itr.next
+            #itr2=itr1
+            #itr1=itr1.next
 
+        print('No node in the linked list has that data')
     def remove_at(self, index):
         if index<0 or index>=self.get_length():
             raise Exception("Invalid Index")
@@ -87,44 +114,13 @@ class LinkedList:
             self.insert_at_end(data)
 
 
-    def insert_after_value(self, data_after, data_to_insert):
-        if self.head is None:
-            return
-
-        if self.head.data==data_after:
-            self.head.next = Node(data_to_insert,self.head.next)
-            return
-
-        itr = self.head
-        while itr:
-            if itr.data == data_after:
-                itr.next = Node(data_to_insert, itr.next)
-                break
-
-            itr = itr.next
-
-    def remove_by_value(self, data):
-        if self.head is None:
-            return
-
-        if self.head.data == data:
-            self.head = self.head.next
-            return
-
-        itr = self.head
-        while itr.next:
-            if itr.next.data == data:
-                itr.next = itr.next.next
-                break
-            itr = itr.next
-
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_values(["banana","mango","grapes","orange"])
     ll.print()
-    ll.insert_after_value("mango","apple")
+    ll.insert_after_value("mango","apple") # insert apple after mango
     ll.print()
-    ll.remove_by_value("orange")
+    ll.remove_by_value("orange") # remove orange from linked list
     ll.print()
     ll.remove_by_value("figs")
     ll.print()
@@ -133,8 +129,4 @@ if __name__ == '__main__':
     ll.remove_by_value("apple")
     ll.remove_by_value("grapes")
     ll.print()
-
-    # ll.insert_values([45,7,12,567,99])
-    # ll.insert_at_end(67)
-    # ll.print()
 

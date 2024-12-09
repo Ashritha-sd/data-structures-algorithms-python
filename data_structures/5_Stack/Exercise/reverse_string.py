895cc35e46ba0c6ec60cbfc12dd15dd8c2af48a2
My name <me@example.com>
2024-12-09 09:42:48 -0600

    My solution to Stack exercise

--- data_structures/5_Stack/Exercise/reverse_string.py
+++ data_structures/5_Stack/Exercise/reverse_string.py
@@ -1,37 +1,39 @@
-from collections import deque
+#!/usr/bin/env python
+# coding: utf-8
 
+# In[32]:
+
+
+from collections import deque
 class Stack:
     def __init__(self):
         self.container = deque()
-
-    def push(self, val):
+    
+    def push(self,val):
         self.container.append(val)
-
+        
     def pop(self):
         return self.container.pop()
-
+    
     def peek(self):
-        return self.container[-1]
-
+        return  self.container[-1]
+    
     def is_empty(self):
-        return len(self.container) == 0
-
+        return len(self.container)==0
+    
     def size(self):
         return len(self.container)
 
-def reverse_string(s):
-    stack = Stack()
 
-    for ch in s:
-        stack.push(ch)
-
-    rstr = ''
-    while stack.size()!=0:
-        rstr += stack.pop()
-
-    return rstr
+# In[33]:
 
 
-if __name__ == '__main__':
-    print(reverse_string("We will conquere COVI-19"))
-    print(reverse_string("I am the king"))
+s=Stack()
+str="We will conquere COVID-19"
+for i in str:
+    s.push(i)
+rev_str=''
+while not s.is_empty():
+    rev_str+=s.pop()
+print(rev_str)
+
